c4ae4af400cfd4956bf5a835cfaf2ee739ddf77c
benjamin-benesti <benjamin.ben-esti@etu.univ-amu.fr>
2020-03-26 18:55:44 +0100

    test

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import Player from "./Player";
 
 function App() {
   return (
-      /*<div><Field name = "Pseudo" type = "Text"/>
-           <Field name = "Mot de Passe" type = "password"/>
-           <Field name = "Âge" type="number" /></div>*/
+
       <div>
           <div id="HboxPlayer"><Player pseudo="LaTeuTeu" score="1500" boss="true"></Player><Player pseudo="darsk" score="200" boss="false"></Player></div>
           <div id="Canvas"><Canvas id="canvas"></Canvas></div>
