1109f414cac180dfa16d87828d0a7edff59f3342
baptistegasser <baptistegasser36@gmail.com>
2020-03-27 10:10:12 +0100

    Ajout de la base de la page d'acceuil

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import './Home.css';
+import { Container, Button, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 
 class Home extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Guess'Art</h1>
-                <p>A fun online about drawing and mysterious words...</p>
-            </div>
+        <Container className="h-100">
+            <Row xs={1}>
+                <Col className='text-center'>
+                    <h1>Guess Art</h1>
+                </Col>
+                <Col className='text-center'>
+                    <p>A fun online about drawing and mysterious words...</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col className='text-center'>
+                    <Link to="/login">
+                        <Button>login</Button>
+                    </Link>
+                </Col>
+            </Row>
+        </Container>
         );
     }
 };
