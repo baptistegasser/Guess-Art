5a07b23a1ae74557a3fe3203321b786ac39bcd7c
baptistegasser <baptistegasser36@gmail.com>
2020-04-02 11:02:38 +0200

    Suppression des ancien fichier d'ébauche

--- src/store/actions.js
+++
@@ -1,9 +0,0 @@
-export const TYPES = {
-    SET_LOGGED: 'SET_LOGGED',
-    SET_USERNAME: 'SET_USERNAME'
-}
-  
-export const actions = {
-    setLogged: (logged) => ({ type: TYPES.SET_LOGGED, logged }),
-    setUsername: (username) => ({ type: TYPES.SET_USERNAME, username })
-}
