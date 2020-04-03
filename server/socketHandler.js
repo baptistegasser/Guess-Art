3d1304294c830fccc4cd857ef7d7ad69ba551bbc
baptistegasser <baptistegasser36@gmail.com>
2020-04-03 18:32:20 +0200

    Rewrited socketHandler to use RoomList.

--- server/socketHandler.js
+++ server/socketHandler.js
@@ -1,15 +1,9 @@
 const Room = require('./room');
+const RoomList = require('./roomList');
 
-/**
- * @type {Map.<string, Room>}
- */
-const rooms = new Map();
-/**
- * @type {Map.<string, string>}
- */
-const userToRoomMap = new Map();
-
-rooms.set('my_id', new Room('my_id', 4));
+// Mock room, TODO: remove
+const mock_room = new Room('mock_id', 4);
+RoomList.addRoom(mock_room);
 
 function getUsername(socket) {
     return socket.request.session.user.username;
@@ -24,65 +18,54 @@ function join_room(socket, room_id) {
     const username = getUsername(socket);
 
     // Remove the user from the old room
-    if (userToRoomMap.get(username) !== undefined) {
-        leave_room(socket, userToRoomMap.get(username));
+    const old_room = RoomList.getRoomFromUser(username);
+    if (old_room !== undefined) {
+        leave_room(socket);
     }
 
-    const room = rooms.get(room_id);
-    if (room === undefined || !room.hasEmptySlot()) {
+    if (!RoomList.isAvailable(room_id)) {
         return socket.emit('game_error', 'The room is full or the id is not valid');
     }
 
     // Add the user to the room, the socket.io room and him the draw instr
-    room.addUser(username);
-    userToRoomMap.set(username, room_id);
+    RoomList.addUserToRoom(username, room_id);
     socket.join(room_id);
     socket.to(room_id).emit('user_joined', username);
-    socket.emit('draw_instr', room.getDrawInstructions());
+    socket.emit('draw_instr', RoomList.getDrawInstruction(room_id));
 }
 
 /**
  * @param {SocketIO.socket} socket 
  */
-function leave(socket) {
+function leave_room(socket) {
     const username = getUsername(socket);
-    const room_id = userToRoomMap.get(username);
+    const room_id = RoomList.removeUserFromIsRoom(username);
     if (room_id !== undefined) {
-        leave_room(socket, room_id);
+        socket.to(room_id).emit('user_leaved', username);
     }
-}
-
-function leave_room(socket, room_id) {
-    const username = getUsername(socket);
-    rooms.get(room_id).removeUser(username);
-    userToRoomMap.delete(username);
-    socket.to(room_id).emit('user_leaved', username);
-    socket.leave(room_id);
 }
 
 function check_guess(socket, word) {
     const username = getUsername(socket);
-    const room_id = userToRoomMap.get(username);
-    if (room_id === undefined) return;
+    const room = RoomList.getRoomFromUser(username);
 
-    const room = rooms.get(room_id);
+    if (room === undefined) return;
+
     if (room.tryGuess(username, word)) {
         socket.emit('guess_success');
     } else {
-        socket.to(room_id).emit('user_msg', word);
+        socket.to(room.id).emit('user_msg', word);
     }
 }
 
 function handle_draw_instr(socket, draw_instr) {
     const username = getUsername(socket);
-    const room_id = userToRoomMap.get(username);
-    if (room_id === undefined) return;
+    const room = RoomList.getRoomFromUser(username);
 
-    const room = rooms.get(room_id);
-    if (!room.isBoss(username)) return;
+    if (room === undefined || !room.isBoss(username)) return;
 
     room.addDrawInstructions(draw_instr);
-    socket.to(room_id).emit('draw_instr', draw_instr);
+    socket.to(room.id).emit('draw_instr', draw_instr);
 }
 
 /**
@@ -92,9 +75,9 @@ function handle_draw_instr(socket, draw_instr) {
 const socketHandler = (socket) => {
     console.log('A client connected');
 
-    socket.on('disconnect', () => leave(socket));
+    socket.on('disconnect', () => leave_room(socket));
     socket.on('join_room', room_id => join_room(socket, room_id));
-    socket.on('leave_room', room_id => leave_room(socket, room_id));
+    socket.on('leave_room', () => leave_room(socket));
     socket.on('guess', word => check_guess(socket, word));
     socket.on('draw_instr', draw_instr => handle_draw_instr(socket, draw_instr))
 }
