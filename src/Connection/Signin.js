c85dbf858df608e8a1245a4352f685f06a6e14a6
baptistegasser <baptistegasser36@gmail.com>
2020-04-07 12:05:59 +0200

    Rewrited the signin page:  - Use the simpler form  - Removed maping to unused redux state (isLogged)  - Added handling of redirection to a specific page after login  - Made input change handling easier and smaller  - Little bit of doc

--- src/Connection/Signin.js
+++ src/Connection/Signin.js
@@ -6,10 +6,9 @@ import ConnectForm from './ConnectForm';
 import Verification from '../Verification';
 import { signIn } from '../store/actions';
 
-const mapStateToProps = state => ({
-    isLogged: state.isLogged
-});
-
+/**
+ * Map allowing to use the redux action to sign in
+ */
 const mapDispatchToProps = () => {
     return {
         signIn
@@ -23,60 +22,64 @@ class SigninForm extends React.Component{
             username: '',
             password: '',
             rememberMe: false,
-            success: false
+            success: false,
+            destination: '/room', // Default destination is the room creation page
+            error_msg: ''
         };
-        this.FormRef = React.createRef();
 
+        // If the props "location.state.destination" is set, redirect to this specific destination
+        if (this.props.location.state !== undefined && this.props.location.state.destination !== undefined) {
+            this.state.destination = this.props.location.state.destination;
+            this.state.error_msg = 'You need to login to access this page !';
+        }
+
+        this.handleChange = this.handleChange.bind(this);
         this.checkAndSubmit = this.checkAndSubmit.bind(this);
-        this.handleUsernameChange = this.handleUsernameChange.bind(this);
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
-        this.handleRememberMeChange = this.handleRememberMeChange.bind(this);
     }
 
-    handleUsernameChange(event) {
-        this.setState({ username: event.target.value });
-    }
-
-    handlePasswordChange(event) {
-        this.setState({ password: event.target.value });
-    }
-
-    handleRememberMeChange(event) {
-        this.setState({ rememberMe: event.target.checked });
+    /**
+     * Handle the changes of the input fields
+     */
+    handleChange(event) {
+        if (event.target.type === 'checkbox') {
+            this.setState({ [event.target.id]: event.target.checked });
+        } else {
+            this.setState({ [event.target.id]: event.target.value });
+        }
     }
 
     render() {
         // If the login was successfull, redirect
         if (this.state.success) {
-            return <Redirect to='/room'/>
+            return <Redirect to={ this.state.destination } />
         }
 
         return (
-        <ConnectForm onSubmit={this.checkAndSubmit} submit_text='Sign in' ref={this.FormRef}>
-            <Form.Group controlId='UsernameGroup'>
+        <ConnectForm onSubmit={this.checkAndSubmit} submit_text='Sign in' error_msg={this.state.error_msg}>
+            <Form.Group controlId='username'>
                 <Form.Label>Username</Form.Label>
                 <Form.Control
                     required
                     type="text"
                     placeholder="Enter your Username"
                     value={this.state.username}
-                    onChange={this.handleUsernameChange}/>
+                    onChange={this.handleChange}/>
             </Form.Group>
-            <Form.Group controlId='PasswordGroup'>
+            <Form.Group controlId='password'>
                 <Form.Label>Password</Form.Label>
                 <Form.Control
                     required
                     type="password"
                     placeholder="Password"
                     value={this.state.password}
-                    onChange={this.handlePasswordChange}/>
+                    onChange={this.handleChange}/>
             </Form.Group>
-            <Form.Group controlId='RememberMeGroup'>
+            <Form.Group controlId='rememberMe'>
                 <Form.Check
                     type='switch'
                     label='Remember me'
                     value={this.state.rememberMe}
-                    onChange={this.handleRememberMeChange}/>
+                    onChange={this.handleChange}/>
             </Form.Group>
             <Form.Group>
                 <Link to='/signup'>I don't have an account</Link>
@@ -85,6 +88,9 @@ class SigninForm extends React.Component{
         )
     }
 
+    /**
+     * Check the datas before submiting the form
+     */
     async checkAndSubmit(event) {
         event.preventDefault();
         event.stopPropagation();
@@ -96,13 +102,15 @@ class SigninForm extends React.Component{
 
         // Ensure the fields are valid
         if (!Verification.checkString(username, 'Username') || !Verification.checkString(password, 'Password')) {
-            this.FormRef.current.displayError(Verification.getMessage());
-            return;
+            return this.setState({error_msg: Verification.getMessage() });
         }
 
         await this.submitForm(username, password, rememberMe);
     }
 
+    /**
+     * Handle the form submition to the API and the response
+     */
     async submitForm(username, password, rememberMe) {
         // Create the and send the signin request
         var data = new URLSearchParams();
@@ -118,25 +126,24 @@ class SigninForm extends React.Component{
             credentials: 'same-origin',
         });
 
-        // If response is ok set success and signed in
+        // If response is ok set success and signIn (Redux)
         if (response.ok) {
             this.props.signIn();
             this.setState({ success: true });
         } else {
             let message = 'Unknown error while sign in. If it keep happening, please contact an admin.';
 
-            // If it's not a server error, retrieve the message
+            // If it's not a server error, retrieve the error message and diplay it
             if (response.status !== 500) {
                 const json = await response.json();
                 message = json.message;
             }
-
-            this.FormRef.current.displayError(message);
+            this.setState({error_msg: message });
         }
     }
 }
 
 export default connect(
-    mapStateToProps,
+    null,
     mapDispatchToProps()
 )(SigninForm);
