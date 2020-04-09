1d6ee4c668b5a02d61ecdeb8bcce0a90b09a218b
baptistegasser <baptistegasser36@gmail.com>
2020-04-09 19:04:56 +0200

    Added api call for room creation

---
+++ server/room_creation.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const Room = require('./room');
+const IDGenerator = require('./id_generator');
+const RoomList = require('./roomList');
+
+router.post('/room/create', (req, res) => {
+    // Check connected
+    if (!req.session.user) {
+        return res.status(403).send();
+    }
+
+    const settings = {
+        max_player: 8,
+        min_player_start: 4,
+        round_duration: 45,
+        round_count: 10,
+    }
+
+    try {
+        const id = IDGenerator.getNewID();
+        const room = new Room(id, settings);
+        RoomList.addRoom(room);
+        res.status(200).send({ id: id });
+    } catch (e) {
+        if (e instanceof Error) {
+            res.status(400).send({ message: e.message });
+        } else {
+            res.status(500).send({ message: 'Unknown server error.' });
+        }
+    }
+})
+
+module.exports = router;
