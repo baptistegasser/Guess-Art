ec6409c1cacd5584eb46a2545c33e9e1a9416223
baptistegasser <baptistegasser36@gmail.com>
2020-04-14 11:46:44 +0200

    Fix problème import ES6/ES5 by duplicating file, no other solution found

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -1,4 +1,4 @@
-const Verification = require('../../../src/verification_src');
+const Verification = require('../../verification');
 const router = require('express').Router();
 const Bcrypt = require('bcryptjs');
 const UserModel = require('../../model/user');
