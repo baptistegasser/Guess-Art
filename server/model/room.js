73c2db350ba5f605a3263992252dcd4e90341b76
baptistegasser <baptistegasser36@gmail.com>
2020-04-17 16:42:28 +0200

    Fixed addUsername

--- server/model/room.js
+++ server/model/room.js
@@ -120,7 +120,7 @@ class Room {
         this.broadcastFrom(socket, 'user_joined', this._gameHandler.getPlayer(socket));
         socket.emit('draw_instr', this._gameHandler.getDrawInstr());
         socket.emit('game_info', { draw_instr: this._gameHandler.getDrawInstr(), players: this._gameHandler.getAllPlayers() });
-        this._gameHandler.addUser(socket);
+        this._gameHandler.addUser(socket, this.getUsername(socket));
     }
 
     removeClient(socket) {
