2084ae1bf48015d88cbafd086d5dedd281e1b060
baptistegasser <baptistegasser36@gmail.com>
2020-04-20 11:57:20 +0200

    Mapped the redux functions to RoomComponenent

--- src/Room/RoomComponent.js
+++ src/Room/RoomComponent.js
@@ -1,18 +1,54 @@
 import React from 'react';
 import {connect} from "react-redux";
+import {setBoss, setIsBoss, addPlayer, removePlayer, setTool, setWidth, setColor} from '../store/actions/room';
 
 const mapStateToProps = state => ({
     roomInfo: state.roomInfo
-})
+});
+
+const mapDispatchToProps = () => {
+    return {
+        setBoss,
+        setIsBoss,
+        addPlayer,
+        removePlayer,
+        setTool,
+        setWidth,
+        setColor
+    };
+};
 
 class RoomComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log(props);
+    setBoss() {
+        this.props.setBoss();
+    }
+
+    setIsBoss() {
+        this.props.setIsBoss();
+    }
+
+    addPlayer() {
+        this.props.addPlayer();
+    }
+
+    removePlayer() {
+        this.props.removePlayer();
+    }
+
+    setTool() {
+        this.props.setTool();
+    }
+
+    setWidth() {
+        this.props.setWidth();
+    }
+
+    setColor() {
+        this.props.setColor();
     }
 }
 
 export default connect(
     mapStateToProps,
-    null,
+    mapDispatchToProps(),
 )(RoomComponent);
