acb8f44c3ad6de5271d4fb71fd9658d2bde22b1f
baptistegasser <baptistegasser36@gmail.com>
2020-04-20 16:23:53 +0200

    Removed toolbar handling from the Room to use the ToolBar class

--- src/Room.js
+++ src/Room.js
@@ -3,12 +3,11 @@ import './Room.css';
 import Canvas from "./Canvas.js";
 import PlayerList from './Room/Players/PlayerList'
 import Chrono from "./Chrono";
-import {Container} from "react-bootstrap";
-import {Col} from "react-bootstrap";
-import {Row} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import Chat from "./Room/Chat/Chat";
 import {getUsername} from './store/actions'
 import {connect} from "react-redux";
+import ToolBar from './Room/ToolBar/ToolBar'
 const socketIo = require("socket.io-client");
 
 const mapStateToProps = state => (
@@ -23,89 +22,17 @@ const mapDispatchToProps = () => {
     };
 };
 
-
-
-
 class Room extends React.Component {
     constructor(props) {
         super(props);
         this.socket = socketIo()
         this.state = {color:"",tool:"",width:"",boss:""}
-        this.clickColor =this.clickColor.bind(this)
-        this.clickWidth =this.clickWidth.bind(this)
-        this.clickTool = this.clickTool.bind(this)
         this.leaveRoom = this.leaveRoom.bind(this)
-    }
-
-    clickColor(e)
-    {
-        var allButtons = document.getElementsByClassName("color")
-        for (let i = 0; i <allButtons.length;i++)
-        {
-            allButtons.item(i).setAttribute("class","color")
-        }
-        e.target.className = "color selected"
-        this.setState({color : ""+e.target.id})
-        var eraser = document.getElementById("eraser")
-        eraser.setAttribute("class","tool")
-        if (this.state.tool === "eraser")
-            this.setState({tool:""})
-
-    }
-
-    clickWidth(e)
-    {
-        var allButtons = document.getElementsByClassName("width")
-        for (let i = 0; i <allButtons.length;i++)
-        {
-            allButtons.item(i).setAttribute("class","width")
-        }
-        e.target.className = "width selected"
-        this.setState({width : ""+e.target.id})
-        var bucket = document.getElementById("bucket")
-        bucket.setAttribute("class","tool")
-        if (this.state.tool === "bucket")
-            this.setState({tool:""})
-    }
-
-    clickTool(e)
-    {
-        var allButtons = document.getElementsByClassName("tool")
-        for (let i = 0; i <allButtons.length;i++)
-        {
-            allButtons.item(i).setAttribute("class","tool")
-        }
-        e.target.className = "tool selected"
-        this.setState({tool : ""+e.target.id})
-        if (e.target.id === "eraser")
-        {
-            var allColors = document.getElementsByClassName("color")
-            for (let i = 0; i <allColors.length;i++)
-            {
-                allColors.item(i).setAttribute("class","color")
-            }
-            this.setState({color:""})
-        }
-        if (e.target.id === "bucket")
-        {
-            var allWidths = document.getElementsByClassName("width")
-            for (let i = 0; i <allWidths.length;i++)
-            {
-                allWidths.item(i).setAttribute("class","width")
-            }
-            this.setState({width:""})
-        }
-
     }
 
     leaveRoom()
     {
         this.socket.emit("leave_room");
-    }
-
-    clearCanvas()
-    {
-        //TODO reformater draw-instr pour clear canvas
     }
 
     /**
@@ -118,11 +45,6 @@ class Room extends React.Component {
     }
 
     render(){
-        //let tabPlayers = {1:{pseudo:"ben",score:1500,boss:true},2:{pseudo:"baptiste",score:1800,boss:false}}
-
-
-
-
         let boss = false;
         console.log(this.state.boss)
         console.log(this.props.user)
@@ -133,48 +55,6 @@ class Room extends React.Component {
         }
 
         console.log("BOSS ="+boss )
-
-
-        let tabColors = ['rgb(255, 0, 0)','rgb(0, 0, 255)','rgb(0, 255, 0)','rgb(102, 51, 0)','rgb(255, 255, 0)','rgb(255, 102, 204)','rgb(0,0,0)','rgb(255, 255, 255)','rgb(255, 102, 0)','rgb(204, 0, 153)','rgb(113, 113, 113)']
-        let selectorColor = tabColors.map(col=>{
-            if (this.state.color === "" && this.state.tool === "")
-            {
-                if (col === 'rgb(0,0,0)')
-                    return(<button className="color selected" key={col} id={col} style={{backgroundColor: col}} onClick={this.clickColor}/>)
-            }
-                return(<button className="color" key={col} id={col} style={{backgroundColor: col}} onClick={this.clickColor}/>)
-        });
-
-        let tabWidth = [1,5,10,15,20];
-        let selectorWidth = tabWidth.map(val=>{
-            let picture = process.env.PUBLIC_URL + '/width/taille_'+val+'.jpg';
-            if (this.state.width === "")
-            {
-                if (val === 1)
-                    return(<button className="width selected" id={val} key={val} onClick={this.clickWidth} style={{backgroundImage: 'url(' + picture + ')'}}/>)
-            }
-            return(<button className="width " key={val} id={val}  onClick={this.clickWidth} style={{backgroundImage: 'url(' + picture + ')'}}/>)
-        });
-
-        let tabTool = ['eraser','bucket','trash'];
-        let selectorTool = tabTool.map(val=>{
-            let picture = process.env.PUBLIC_URL + '/tool/'+val+'.png';
-            if (val === 'trash')
-            {
-                return (<button className="trash"  onClick={this.clearCanvas} style={{backgroundImage: 'url('+picture+')'}}/>)
-            }
-            else
-                return(<button className="tool"  key={val} id={val} onClick={this.clickTool} style={{backgroundImage: 'url('+picture+')'}}/>)
-        });
-
-
-
-        if (boss === false)
-        {
-            selectorColor = ""
-            selectorTool = ""
-            selectorWidth = ""
-        }
         return (<Container fluid>
                 <Row style={{margin:0}}>
                     <Col xs={4}><Chrono socket = {this.socket} /></Col>
@@ -187,19 +67,15 @@ class Room extends React.Component {
                     <Col xs={2}><Chat socket={this.socket} boss={boss}/></Col>
                 </Row>
                 <Row style={{margin:0}}>
-                    <Col xs={3}/>
-                    <Col xs={7}><div>{selectorColor}</div></Col>
-                </Row>
-                <Row style={{margin:0}}>
-                    <Col xs={3}/>
-                    <Col xs={7}><div>{selectorWidth}{selectorTool}</div></Col>
+                    <Col xs={{ span: 7, offset: 3 }}>
+                        {boss ? <ToolBar></ToolBar> : '' }
+                    </Col>
                 </Row>
 
             </Container>)
         }
 
 }
-
 
 export default connect(
     mapStateToProps,
