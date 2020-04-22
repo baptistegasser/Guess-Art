0ea8d0d013d5d204de81205633565ed8a3bef7ea
baptistegasser <baptistegasser36@gmail.com>
2020-04-22 11:40:16 +0200

    Fix / Enhancement: Modified Chronos class (see more below) - cleaned the code - extracted the css from the jsx and into Chrono.css - changed the style to correctly see the gif - fixed memory leak due to interval been called after component was unmounted - modified autostart feature to launch only if a duration is provided by props.duration - removed socket handling to make the class reusable

--- src/Room/Chrono/Chrono.js
+++ src/Room/Chrono/Chrono.js
@@ -1,46 +1,62 @@
 import React from "react";
+import './Chrono.css';
 
-class Chrono extends React.Component
-{
+class Chrono extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {second : 30}
-        this.props.socket.on('start_round',(data) => this.updateChrono(data))
-        this.updateChrono = this.updateChrono.bind(this)
-        this.launchChrono = this.launchChrono.bind(this)
+        this.state = {
+            started: false,
+            autoStart: props.duration > 0,
+            remaining: props.duration || 0
+        };
+
+        this.chronoTimeout = undefined;
+        this.stopChrono = this.stopChrono.bind(this);
+        this.startChrono = this.startChrono.bind(this);
+        this.updateChrono = this.updateChrono.bind(this);
     }
 
-    updateChrono(data)
-    {
-        this.setState({second : data})
-        this.launchChrono()
+    startChrono(duration) {
+        if (this.state.started) return;
+
+        this.setState({ started: true, remaining: duration });
+        this.chronoTimeout = setInterval(this.updateChrono(), 1000);
     }
 
-    launchChrono()
-    {
-
-        var chrono = setInterval(function () {
-            if (this.state.second > 0)
-                this.setState({second: this.state.second -1})
-            else
-            {
-                clearInterval(chrono)
-            }
-
-        }.bind(this),1000)
-
-
-
+    updateChrono() {
+        if (this.state.remaining > 0) {
+            this.setState({ remaining: this.state.remaining -1 });
+        } else {
+            this.stopChrono();
+        }
     }
+
+    stopChrono() {
+        if (!this.state.started || this.chronoTimeout === undefined) return;
+
+        this.setState({ started: false });
+        clearInterval(this.chronoTimeout);
+        this.chronoTimeout = undefined;
+    }
+
     componentDidMount() {
-        this.setState({second : 5})
-        this.launchChrono()
+        if (this.state.autoStart) {
+            this.startChrono();
+        }
+    }
+
+    /**
+     * Clear the interval to prevent memory leaks
+     */
+    componentWillUnmount() {
+        this.stopChrono();
     }
 
     render() {
         return (
-            <div style={{width:"60px",height:"50px"}}><p style={{background:"rgba(255,255,255,0)",backgroundImage:'url(https://media.giphy.com/media/YWmGm0UFfYnQs/giphy.gif)'}} >{this.state.second}</p></div>
-
+            <div className='chrono'>
+                {this.state.started ? <p>{this.state.second}</p> : ''}
+            </div>
         )
     }
 }
