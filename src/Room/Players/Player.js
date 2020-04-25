5799e43aeb07bc7a4a5d452c9458e7ab727c3611
benjamin-benesti <benjamin.ben-esti@etu.univ-amu.fr>
2020-04-25 23:46:21 +0200

    surbrillance en vert de la player list ainsi que up du chat pour tous quand il y a un nouveau guesser

--- src/Room/Players/Player.js
+++ src/Room/Players/Player.js
@@ -7,7 +7,12 @@ class Player extends React.Component
     render() {
         const bossImg = <img className="boss-icon" src="https://media.giphy.com/media/QXJAPsp0Q35qlOqMUy/giphy.gif" alt="Boss"/>;
 
-        return <div class="player">
+        let idName ='normal'
+        if (this.props.hasGuessed)
+            idName = 'guessed'
+
+
+        return <div class="player" id={idName}>
             <Row xs={1}>
                 <Col xs={12}>
                     <h4>{this.props.pseudo}</h4>
